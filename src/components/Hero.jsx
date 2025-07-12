c5d5addfa1cd35ebd000bae5fe6ad05fa55911e1
Uzairimam03 <uzair.imam03@gmail.com>
2025-07-12 23:38:34 +0500

    Download Resume added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,7 @@
-import { animate, motion } from "framer-motion"
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { animate, motion } from "framer-motion";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
-
 
 const fadeInUp = {
   initial: { opacity: 0, y: 20 },
@@ -18,66 +17,89 @@ const staggerContainer = {
   },
 };
 
-
 export const Hero = () => {
-  return <motion.section 
-  id="home" 
-  className= "hero"
-  initial={{opacity: 0}} 
-  animate={{opacity:1}}
-  transition={{duration:0.8, delay:0.2}}
-  >
-    <div className="hero-container">
- 
-    <motion.div className="hero-content" variants={staggerContainer} initial = "initial" animate= "animate">
-      <motion.div className="hero-badge">
-        <span>👋 Hello, I'm</span>
-      </motion.div>
-      <motion.h1 className="glitch" variants={fadeInUp} whileHover={{scale: 1.02}}>
-     Muhamad Uzair Imam
-      </motion.h1>
-      <motion.h2 className="hero-subtitle"  variants={fadeInUp}>
-        Creative MERN Stack Developer
-      </motion.h2>
-      <motion.p className="hero-description"  variants={fadeInUp}>
-         Full Stack Engineer with a strong problem-solving mindset and a passion for continuous learning. Dedicated to exploring emerging technologies and building impactful real-world applications.
-      </motion.p>
+  return (
+    <motion.section
+      id="home"
+      className="hero"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8, delay: 0.2 }}
+    >
+      <div className="hero-container">
+        <motion.div
+          className="hero-content"
+          variants={staggerContainer}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.div className="hero-badge">
+            <span>👋 Hello, I'm</span>
+          </motion.div>
+          <motion.h1
+            className="glitch"
+            variants={fadeInUp}
+            whileHover={{ scale: 1.02 }}
+          >
+            Muhamad Uzair Imam
+          </motion.h1>
+          <motion.h2 className="hero-subtitle" variants={fadeInUp}>
+            Creative MERN Stack Developer
+          </motion.h2>
+          <motion.p className="hero-description" variants={fadeInUp}>
+            Full Stack Engineer with a strong problem-solving mindset and a
+            passion for continuous learning. Dedicated to exploring emerging
+            technologies and building impactful real-world applications.
+          </motion.p>
 
-       <motion.div className="cta-buttons" variants={staggerContainer}>
-            <motion.a
-              href="#projects"
-              className="cta-primary"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              {" "}
-              View My Work
-            </motion.a>
-            <motion.a
-              href="#contact"
-              className="cta-secondary"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              Contact Me
-            </motion.a>
+          <motion.div className="cta-buttons" variants={staggerContainer}>
+         
+              <motion.a
+                href="./Uzair-Imam-CV.pdf"
+                className="cta-secondary w-full sm:w-auto text-center"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                Download Resume
+              </motion.a>
+
+              <motion.a
+                href="#projects"
+                className="cta-primary w-full sm:w-auto text-center"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                View My Work
+              </motion.a>
+
+              <motion.a
+                href="#contact"
+                className="cta-secondary w-full sm:w-auto text-center"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                Contact Me
+              </motion.a>
+            {/* </div> */}
           </motion.div>
           <motion.div className="social-links" variants={staggerContainer}>
             <motion.a href="https://github.com/Uzairimam03" target="_blank">
               <i className="fab fa-github"> </i>
             </motion.a>
-            <motion.a href="https://www.linkedin.com/in/muhammad-uzair-imam-b29934372/" target="_blank">
+            <motion.a
+              href="https://www.linkedin.com/in/muhammad-uzair-imam-b29934372/"
+              target="_blank"
+            >
               <i className="fab fa-linkedin"> </i>
             </motion.a>
 
-             <motion.a href="https://www.twitter.com" target="_blank">
+            <motion.a href="https://www.twitter.com" target="_blank">
               <i className="fab fa-twitter"> </i>
             </motion.a>
-          
           </motion.div>
         </motion.div>
-    
-          <motion.div
+
+        <motion.div
           className="hero-image-container"
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
@@ -132,8 +154,7 @@ export const Hero = () => {
             </div>
           </motion.div>
         </motion.div>
-          
-    </div>
-
-  </motion.section>
-}
+      </div>
+    </motion.section>
+  );
+};
